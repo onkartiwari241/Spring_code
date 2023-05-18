a1ce35d829a2b1b72f1a25d17a927fe38ea29b94
onkarengagebay <onkar@engagebay.co>
2023-05-18 12:48:58 +0530

    Fourth commit

---
+++ learn-spring-orm/src/test/java/com/lcwd/orm/learnspringorm/LearnSpringOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcwd.orm.learnspringorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
